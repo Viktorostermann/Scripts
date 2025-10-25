416585fdb38df5c9cded547dd389bb165faae2f2
Victor Miletic <viktoremiletic@gmail.com>
2025-10-25 16:28:12 -0500

    Actualizacion

---
+++ Python_Avanzado/Tema_3/P.O.O/Clases_Objetos.py
@@ -0,0 +1,18 @@
+'''Los metodos son "Funciones" que 
+   estan asociadas a su vez a otra 
+   Funcion Principal / GLobal) 
+   que tiene una Clase u Objeto '''
+
+
+# Para crear una clase se debe realizar los siguiente:
+
+# 1. Definir la clase ---> Nombre_Clase = (Objetos Contenedores)
+# 2. Definir los metodos ---> __init__ = (Inicializacion de Funciones)
+# 3. Definir los atributos ---> Caracteristicas propias del objeto = (Variables)
+# 4. Definir 
+
+# El metodo __init__ inicializa los atributos de una Clase o Funcion para poder utilizarlos
+
+# Desarrollar un programa que haga una suma simple.
+    
+
