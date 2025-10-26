b7371d2fa325e533eeb9bdfd7b89fc8f865cf77c
Victor Miletic <viktoremiletic@gmail.com>
2025-10-25 23:57:48 -0500

    Actualizacion de Clases y Objetos en Python

--- Python_Avanzado/Tema_3/P.O.O/Clases_Objetos.py
+++ Python_Avanzado/Tema_3/P.O.O/Clases_Objetos.py
@@ -9,16 +9,27 @@
 # 1. Definir la clase ---> Nombre_Clase = (Objetos Contenedores)
 # 2. Definir los metodos ---> __init__ = (Inicializacion de Funciones)
 # 3. Definir los atributos ---> Caracteristicas propias del objeto = (Variables)
-# 4. Definir 
+# 4. Definir los objetos ---> Instanciar la Clase = (Crear Objetos)
 
 # El metodo __init__ inicializa los atributos de una Clase o Funcion para poder utilizarlos
 
 # Desarrollar un programa que haga una suma simple
 
 class Suma:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    def __init__(self, valor_1, valor_2):
+        self.valor_1 = valor_1
+        self.valor_2 = valor_2
 
-    def suma(self):
-        return self.a + self.b
+    def sumar(self):
+        return self.valor_1 + self.valor_2
+
+# Crear un objeto de la clase Suma
+operacion = Suma(5, 10)
+
+# Acceder a los atributos del objeto
+print("")
+print(f" El valor del Objeto 1 es: {operacion.valor_1}")  # Salida: El valor 1 es: 5, y el valor 2 es: 10  print(operacion.valor_1)  # Salida: 5
+print(f" El valor del Objeto 2 es: {operacion.valor_2}")  # Salida: El valor 1 es: 5, y el valor 2 es: 10  print(operacion.valor_2)  # Salida: 10
+print("")
+print(f" La suma de los dos objetos es: {operacion.sumar()}")  # Salida: La suma de los dos objetos es: 15 los objetos ---> Instanciar la Clase = (Crear Objetos)
+print("")
