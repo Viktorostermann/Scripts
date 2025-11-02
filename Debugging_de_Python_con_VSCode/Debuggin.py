8e2b3604d46825456b6516dfb02e2f417ef741b4
Victor Miletic <viktoremiletic@gmail.com>
2025-11-02 09:39:45 -0500

    Actualizacion repo

---
+++ Debugging_de_Python_con_VSCode/Debuggin.py
@@ -0,0 +1,11 @@
+# Crear un script que me permita realizar una muestra para debuggear como ejemplo de busqueda de archivos en el sistema operativo
+
+import os
+import sys
+
+ruta = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Debuggin.json")
+print(ruta)
+with open(ruta, 'r') as archivo:
+    print(archivo.read())
+
+print("Fin del programa")
