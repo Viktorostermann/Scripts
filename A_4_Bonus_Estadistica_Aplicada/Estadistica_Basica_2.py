4c864ab1e09fd0249b0982be0ef5eecad31570bb
Victor Miletic <viktoremiletic@gmail.com>
2025-11-16 01:13:21 -0500

    actualizacion

--- A_4_Bonus_Estadistica_Aplicada/Estadistica_Basica_2.py
+++ A_4_Bonus_Estadistica_Aplicada/Estadistica_Basica_2.py
@@ -2,11 +2,22 @@ from collections import Counter
 import numpy as np
 import pandas as pd
 
+import pandas as pd
+
+# tu archivo usa punto y coma como separador
+df = pd.read_csv("/home/viktore/Documentos/2-Python/A_4_Bonus_Estadistica_Aplicada/salary_data.csv", sep=";")
+
+print("")
+print("✅ Archivo cargado correctamente")
+print("")
+print(df.head())
+print("")
+
 def calculo(vector):
     vector=np.array(vector)
 
 
 vector = [1,2,3,4,5,6,7,8,9,8,6,5,7,5]
 
-# Calculo de Vector
-df = pd.read_csv("salary_data.csv")
+calculo(vector)
+print("")
